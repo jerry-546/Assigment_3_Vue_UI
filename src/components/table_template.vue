69fe3fba57e63c5acea13c7fd444335e400672ab
jerry-546 <gonzalez.jerry@heb.com>
2019-07-15 15:22:02 -0500

    cleaned up code

--- src/components/table_template.vue
+++ src/components/table_template.vue
@@ -15,7 +15,6 @@
       <md-table-empty-state
         md-label="No users found"
         :md-description="`No user found for this '${search}' query. Try a different search term or create a new user.`">
-
       </md-table-empty-state>
 
       <md-table-row slot="md-table-row" slot-scope="{ item }">
@@ -73,6 +72,7 @@ export default {
 
      searchOnTable(){
        this.searched = searchByName(this.rows, this.search)
+
      },
 
      removeApper(){
